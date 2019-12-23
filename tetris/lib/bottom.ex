a594dd1204003d45a510592811ee15c644c5ed3f
Bruce Tate <bruce@rapidred.com>
2019-12-23 07:51:53 -0500

    test out of bounds

--- tetris/lib/bottom.ex
+++ tetris/lib/bottom.ex
@@ -7,7 +7,7 @@ defmodule Tetris.Bottom do
   
   def collides?(bottom, {x, y, _color}), do: collides?(bottom, {x, y})
   def collides?(bottom, {x, y}) do
-    !!Map.get(bottom, {x, y})
+    !!Map.get(bottom, {x, y}) || x < 1 || x > 10 || y > 20
   end
   def collides?(bottom, points) when is_list(points) do
     Enum.any?(points, &collides?(bottom, &1))
--- tetris/test/bottom_test.exs
+++ tetris/test/bottom_test.exs
@@ -14,6 +14,18 @@ defmodule BottomTest do
     refute collides?(bottom, [{1, 2, :red}, {1, 3, :red}])
   end
   
+  test "various collisions with top and lower margin" do
+    bottom = %{}
+    
+    assert collides?(bottom, {0, 1})
+    refute collides?(bottom, {1, 2})
+    assert collides?(bottom, {11, 1, :blue})
+    assert collides?(bottom, {1, 21, :red} )
+    refute collides?(bottom, {1, 20, :red})
+    assert collides?(bottom, [{11, 2, :red}, {10, 1, :red}])
+    refute collides?(bottom, [{1, 2, :red}, {1, 3, :red}])
+  end
+  
   test "simple merge test" do
     bottom = %{{1, 1} => {1, 1, :blue}}
     
